4a98a688029789c7741a1af375bda3b868761990
Robert Pooley <robertcpooley@gmail.com>
2026-01-12 20:40:11 -0800

    fix unstage for first commit

--- src/git/commands/unstage.ts
+++ src/git/commands/unstage.ts
@@ -3,13 +3,13 @@ import { GitUtil } from '../gitUtil';
 import { status } from './status';
 
 export async function unstage(args: string[]): Promise<string> {
-  await Util.multiCommand(args, (args, file) => {
-    console.log(`# Unstaging '${file.name}'`);
+    await Util.multiCommand(args, (args, file) => {
+        console.log(`# Unstaging '${file.name}'`);
 
-    const cmd = file.type === 'A' ? 'rm --cached' : 'reset HEAD';
+        const cmd = file.data.type === 'A' ? 'rm --cached' : 'reset HEAD';
 
-    return GitUtil.git(`${cmd} ${file.name}`);
-  });
+        return GitUtil.git(`${cmd} ${file.name}`);
+    });
 
-  return await status([]);
+    return await status([]);
 }
